be0f00c2338989bfde9939dc90518a66ab64b373
jjulian61 <104041355+jjulian61@users.noreply.github.com>
2025-06-02 23:41:32 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import Discounted from "./Components/Discounted";
 import Explore from "./Components/Explore";
 import Featured from "./Components/Featured";
 import Footer from "./Components/Footer";
+import React, { useState } from "react";
 import Highlights from "./Components/Highlights";
 import Landing from "./Components/Landing";
 import Nav from "./Components/Nav";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Switch } from "react-router-dom/cjs/react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"; 
 import Home from "./pages/Home";
 import Books from "./pages/Books"
+import { books } from "../src/data"
+import BookInfo from "./pages/BookInfo"
+import Cart from "./pages/Cart"
+import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
   return (
