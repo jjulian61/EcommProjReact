c6855c91f8ffe5dc9d26f804289d7a58b9a767a1
jjulian61 <104041355+jjulian61@users.noreply.github.com>
2025-06-06 18:56:11 -0400

    Update Price.jsx

---
+++ src/Components/ui/Price.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const price = ({ salePrice,originalPrice}) => {
+  return (
+    <div className="book__price">
+        {
+            salePrice 
+            <>
+            <span className="book__price--normal">
+              ${book.originalPrice.toFixed(2)}
+              </span>
+            ${book.salePrice.toFixed(2)}
+            </>
+            : (
+                <>${originalPrice}</>
+        )}
+        
+      </div>
+  )
+}
+
+export default price
