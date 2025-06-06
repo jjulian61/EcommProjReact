d88d848a17f31e1ce44617ac3f81357d60dee725
jjulian61 <104041355+jjulian61@users.noreply.github.com>
2025-06-06 19:07:34 -0400

    Update Landing.jsx

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -9,9 +9,9 @@ const Landing = () => {
                     <div className="header__description">
                         <h1>Australia'a mostawarded online library platform</h1>
                         <h2>Find your dream book <span className="purple">library</span></h2>
-                        <a href="#features">
+                        <Link to="#features">
                             <button className="btn">Browse Books</button>
-                        </a>
+                        </Link>
                     </div>
                     <figure className="header__img--wrapper">
                         <img src={UndrawBooks} alt="" />
