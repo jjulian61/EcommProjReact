a92f84bb7acca2b482b384c456988a1cbd26f109
jjulian61 <104041355+jjulian61@users.noreply.github.com>
2025-06-18 02:20:35 -0400

    Update Cart.jsx

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,15 +1,18 @@
-import React, { useEffect } from 'react'
-import EmptyCart from"../assets/empty_cart.svg"
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import EmptyCart from "../assets/empty_cart.svg"
 
- const Cart = ({ cart, changeQuantity, cart }) => {
-    const [total, setTotal] = useState();
+ const Cart = ({ cart, changeQuantity, removeItem }) => {
+    const [total, setTotal] = useState(0);
+
     useEffect(() => {
         let price = 0;
         cart.forEach((item) => {
-            price +=  +(item.salePrice || item.originalPrice).toFixed(2);
+            price +=  +((item.salePrice || item.originalPrice) *
+            item.quantity).toFixed(2);
          });
-         setTotal(price)
-    }, [cart, cart.quantity])
+         setTotal(price);
+    }, [cart])
 
 
   return (
@@ -29,9 +32,11 @@ import EmptyCart from"../assets/empty_cart.svg"
                         <div className="cart__body">
                             {cart.map((book) => {
                                     return (
-                                        <div className="cart__item">
+                                        <div className="cart__item" key={book.id}>
                                         <div className="cart__book">
-                                            <img src={book.url}
+                                            <img 
+                                            src={book.url}
+                                            alt={book.title}
                                             className="cart__book--img" 
                                             />
                                             <div className="cart__book--info">
@@ -41,9 +46,11 @@ import EmptyCart from"../assets/empty_cart.svg"
                                                 <span className="cart__book--price">
                                                     ${(book.salePrice || book.originalPrice).toFixed(2)}
                                                 </span>
-                                                <button className="cart__book--remove">
+                                                <button className="cart__book--remove"
+                                                onClick={() => 
+                                                   removeItem(book)} 
+                                                   >
                                                     Remove
-                                                    <span onClick={() => removeItem(book)}></span>
                                                 </button>
                                             </div>
                                         </div>
@@ -54,24 +61,26 @@ import EmptyCart from"../assets/empty_cart.svg"
                                             max={99} 
                                             className="cart__input"
                                             value={book.quantity}
-                                            onChange={(event) => changeQuantity(book, event.target.value)}
+                                            onChange={(event) => 
+                                                changeQuantity(book, event.target.value)}
                                             />
                                         </div>
                                         <div className="cart__total">
                                             $
-                                            {((book.salePrice || book.originalPrice) * book.quantity).toFixed(2)}
+                                            {((book.salePrice || book.originalPrice) 
+                                            * book.quantity).toFixed(2)}
                                         </div>
                                     </div>
                                     )
-                                })
-                            }}
+                                })}
+                                
                         </div>
                         
                             
                         
                        {cart.length === 0 && (
                          <div className="cart__empty">
-                            <img src={EmptyCart} alt="" className="cart__empty--img" />
+                            <img src={EmptyCart} alt="Empty" className="cart__empty--img" />
                             <h2>You don'thave any books in your cart!</h2>
                             <Link to="/books">
                             <button className="btn">Browse Books</button>
@@ -79,14 +88,15 @@ import EmptyCart from"../assets/empty_cart.svg"
                         </div>
                     )}
                     </div>
-                   {cart.length > 0 && <div className="total">
+                   {cart.length > 0 && (
+                    <div className="total">
                         <div className="total__item total__sub-total">
                             <span>Subtotal</span>
-                            <span>${t(otal() * .9).toFixed(2)}</span>
+                            <span>${(total * .9).toFixed(2)}</span>
                         </div>
                         <div className="total__item total__tax">
                             <span>Tax</span>
-                            <span>${(total() * .1).toFixed(2)}</span>
+                            <span>${(total * .1).toFixed(2)}</span>
                         </div>
                         <div className="total__item total__price">
                             <span>Total</span>
@@ -97,7 +107,8 @@ import EmptyCart from"../assets/empty_cart.svg"
                         onClick={() => alert('Havent got around to doing this')}>
                         Proceed to checkout
                         </button>
-                    </div>}
+                    </div>
+                   )}
                 </div>
             </div>
         </main>
