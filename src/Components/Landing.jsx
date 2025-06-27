8345303a89ba49e27dfffb09ba316add1689bcd7
jjulian61 <104041355+jjulian61@users.noreply.github.com>
2025-06-27 00:18:31 -0400

    Update Landing.jsx

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -11,7 +11,7 @@ const Landing = () => {
                     <div className="header__description">
                         <h1>Australia'a most awarded online library platform</h1>
                         <h2>Find your dream book <span className="purple">library</span></h2>
-                        <Link to="#features">
+                        <Link to="/books">
                             <button className="btn">Browse Books</button>
                         </Link>
                     </div>
